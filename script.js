daf4ec4980f80e238a596fbf22990dd6347261b2
andri2621 <andri.setiawan996@gmail.com>
2021-03-18 16:52:30 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+// var nama = prompt('masukkan nama kamu : ')
+
+// alert(`assalamualaikum ${nama}`);
+// alert('udah siap belum main tebak tebakan sama aku?');
+// alert(`peringatan! jangan coba-coba jawab NO! oke?`);
+
+// var sayang = confirm('kamu sayang ga sama aku?')
+
+// if( sayang === true )
+// {
+//     alert('aku juga sayang banget sama kamu');
+// } else {
+//     // alert('jangan gitu dong..')
+//     while (sayang === false) {
+//     alert(`jangan gitu doong.. aku sedih :'(`);
+//     sayang = confirm('sekali lagi.. kamu sayang ga sama aku?')
+//     }
+//     alert('bayu juga sayang banget sama kamu');
+
+// }
+
+// var kangen = confirm('kangen ga sih?')
+
+// if( kangen === true )
+// {
+//     alert('kalo aku sih kangen terusss');
+// } else {
+//     // alert('jangan gitu dong..')
+//     while (kangen === false) {
+//     alert(`masa engga kangennn :'( harus kangen!`);
+//     kangen = confirm('kangen kannn?')
+//     }
+//     alert('kalo aku sih kangen terusss');
+    
+// }
+// alert('aku kangen banget yang setiap malem kita telponan meskipun kadang yang di obrolin gajelas');
+// alert('kangen kamu ngepap yang asal foto tapi bagi aku tetep cantik');
+// alert('kadang dipanggil sayang kamu aja aku kangen');
+// alert(`i love youuu ${nama}`)
+// alert('...');
+// alert('ohiyah aku mau sampein sesuatu ke kamu');
+// alert('sel jaga kesehatan kamu ya');
+// alert('jangan sampe sakit');
+// alert('apalagi kerjaan kamu banyak gini');
+// alert('aku tau kamu sibuk banget');
+// alert('tapi kalo aku bisa..');
+// alert('dan maaf kalo aku egois');
+// alert('kabarin aku yaa');
+// alert('kalo kamu senggang');
